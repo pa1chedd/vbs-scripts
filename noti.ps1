78c9c4507e2da9a69856b08b32c3f8ab8cbef2af
pa1chedd <153358360+pa1chedd@users.noreply.github.com>
2023-12-16 15:00:28 +0100

    Create noti.ps1

---
+++ noti.ps1
@@ -0,0 +1,6 @@
+[reflection.assembly]::loadwithpartialname("System.Windows.Forms")
+[reflection.assembly]::loadwithpartialname("System.Drawing")
+$notify = new-object system.windows.forms.notifyicon
+$notify.icon = [System.Drawing.SystemIcons]::Warning
+$notify.visible = $true
+$notify.showballoontip(10,"2 MINUTEN!","DU HAST NOCH 2 MINUTEN! 😈😈😈",[system.windows.forms.tooltipicon]::Error) 
