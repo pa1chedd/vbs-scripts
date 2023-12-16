b990e70f9d029b7965e972810137bdc5198c4465
pa1chedd <153358360+pa1chedd@users.noreply.github.com>
2023-12-16 15:20:47 +0100

    Update noti.ps1

--- noti.ps1
+++ noti.ps1
@@ -3,4 +3,4 @@
 $notify = new-object system.windows.forms.notifyicon
 $notify.icon = [System.Drawing.SystemIcons]::Warning
 $notify.visible = $true
-$notify.showballoontip(10,"2 MINUTEN!","DU HAST NOCH 2 MINUTEN! `u{1F4A9} `u{1F4A9} `u{1F4A9}",[system.windows.forms.tooltipicon]::Error) 
+$notify.showballoontip(10,"2 MINUTEN!","DU HAST NOCH 2 MINUTEN!",[system.windows.forms.tooltipicon]::Error) 
